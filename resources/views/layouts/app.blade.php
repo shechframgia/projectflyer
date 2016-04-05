282771e1fbe8817befed6f3a605c77ed3374997f
shechframgia <shechaniah.gracen.cantada@framgia.com>
2016-04-05 10:52:52 +0800

    initial

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Project Flyer</title>
+        <link href="{{ url('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Project Flyer</h1>
+@stop
